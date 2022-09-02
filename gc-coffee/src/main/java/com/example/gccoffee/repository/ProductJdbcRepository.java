bb0511406d9eb959b9fedfa9583211991bc15d64
KIM JaeHyeon <rla_wogus@naver.com>
2022-09-02 21:00:40 +0900

    set ProJdbcRepo

---
+++ gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -0,0 +1,73 @@
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.nio.ByteBuffer;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.example.gccoffee.JdbcUtils.toUUID;
+import static com.example.gccoffee.JdbcUtils.toLocalDateTime;
+
+public class ProductJdbcRepository implements ProductRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public ProductJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return jdbcTemplate.query("SELECT * FROM products", productRowMapper);
+    }
+
+    @Override
+    public Product insert(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+
+    @Override
+    public Optional<Product> findById(UUID productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> findByName(String productName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Product> findByCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    private final static RowMapper<Product> productRowMapper = (resultSet, i) -> {
+        var productId = toUUID(resultSet.getBytes("product_id"));
+        var productName = resultSet.getString("product_name");
+        var category = Category.valueOf(resultSet.getString("category"));
+        var price = resultSet.getLong("price");
+        var description = resultSet.getString("description");
+        var createdAt = toLocalDateTime(resultSet.getTimestamp("created_at"));
+        var updatedAt = toLocalDateTime(resultSet.getTimestamp("updated_at"));
+
+        return new Product(productId, productName, category, price, description, createdAt, updatedAt);
+    };
+
+}
--- gc-coffee/src/main/java/com/example/gccoffee/repository/ProductRepository.java
+++ gc-coffee/src/main/java/com/example/gccoffee/repository/ProductRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface Repository {
+public interface ProductRepository {
 
     List<Product> findAll();
 
