d366300bf9178aed5714be2b530de8857aa1c139
KIM JaeHyeon <rla_wogus@naver.com>
2022-09-06 10:13:13 +0900

    set new product insert

--- gc-coffee/src/main/java/com/example/gccoffee/controller/ProductController.java
+++ gc-coffee/src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.gccoffee.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -22,5 +23,20 @@ public class ProductController {
         var products = productService.getAllProducts();
         model.addAttribute("products", products);
         return "product-list";
+    }
+
+    @GetMapping("new-product")
+    public String newProductPage(){
+        return "new-product";
+    }
+
+    @PostMapping("/products")
+    public String newProduct(CreateProductRequest createProductRequest){
+        productService.createProduct(
+                createProductRequest.productName(),
+                createProductRequest.category(),
+                createProductRequest.price(),
+                createProductRequest.description());
+        return "redirect:/products";
     }
 }
